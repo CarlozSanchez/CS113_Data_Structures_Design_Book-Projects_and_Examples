e951268f1126d0e87b07ed473b3c7884758c23df
Carlos Sanchez <Sabad.Sanchez@gmail.com>
2018-04-13 19:19:59 -0700

    Need to Test Char Frequency

--- src/Chapter6/Trees/Driver.java
+++ src/Chapter6/Trees/Driver.java
@@ -25,13 +25,58 @@ public class Driver
 
     private class CharacterFrequencyList
     {
+        int buffer = 5;
         private ArrayList<CharacterFrequency> list;
+
+        public void addCharacter(char c)
+        {
+            for(int i = 0; i < list.size(); i++)
+            {
+                CharacterFrequency charFreq = list.get(i);
+                if(c == charFreq.character)
+                {
+                    charFreq.incrementFrequency();
+                }
+                else
+                {
+                    list.add(new CharacterFrequency(c,1));
+                }
+            }
+        }
+
+        public String toString()
+        {
+            StringBuilder sbA = new StringBuilder();
+            StringBuilder sbB = new StringBuilder();
+
+            for(CharacterFrequency cf : list)
+            {
+                sbA.append(String.format("[%" + buffer + "c]",cf.getCharacter()));
+                sbB.append(String.format("[%" + buffer + "d]", cf.getFrequency()));
+            }
+
+            sbA.append("\n");
+            sbA.append(sbB.toString());
+            return sbA.toString();
+        }
     }
 
     private class CharacterFrequency
     {
         private char character;
         private int frequency;
+
+        public CharacterFrequency()
+        {
+            this.character = 0;
+            this.frequency = 0;
+        }
+
+        public CharacterFrequency(char c,int count)
+        {
+            this.character = c;
+            this.frequency = count;
+        }
 
         public char getCharacter()
         {
@@ -53,7 +98,10 @@ public class Driver
             this.frequency = frequency;
         }
 
-
+        public void incrementFrequency()
+        {
+            this.frequency++;
+        }
     }
 
 
